2ceff229a15d82b4d229f5ec2d0ad2ea90e19358
manutheking <manutheking1991@gmail.com>
2018-06-21 21:35:30 +0200

    Load shaders from class and file

--- ModernOpenGL/main.cpp
+++ ModernOpenGL/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "Constants.hpp"
+#include "Shader.h"
 
 
 int main() {
@@ -18,10 +18,10 @@ int main() {
 
 	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Modern OpenGL", nullptr, nullptr);
 
-	int screenWidth, screenHeight;
+	int screenWidth = 0, screenHeight = 0;
 	
 	// Retina displays
-	glfwGetFramebufferSize(window, &screenHeight, &screenHeight);
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
 
 	if (window == nullptr) {
 		
@@ -41,50 +41,16 @@ int main() {
 
 		return EXIT_FAILURE;
 	}
-
-	glViewport(0, 0, screenWidth, screenHeight);
-
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	GLint success;
-	GLchar infoLog[512];
-
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR -> Vertex shader comp failed: " << infoLog << std::endl;
-	}
-
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR -> Fragment shader comp failed: " << infoLog << std::endl;
-	}
 	
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+	// Define viewport dimensions
+	glViewport( 0, 0, screenWidth, screenHeight );
 
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR -> Program shader comp failed: " << infoLog << std::endl;
-	}
-	
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	Shader ourShader( "core.vs", "core.fs" );
 
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left
-		0.5f, -0.5f, 0.0f,  // right
- 		0.0f, 0.5f, 0.0f	// top
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,	// left + red
+		0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,	// right + blue
+ 		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f		// top + green
 	};
 
 	GLuint VBO, VAO;
@@ -95,8 +61,11 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void *)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), ( void * )( 3* sizeof(GLfloat) ));
+	glEnableVertexAttribArray( 1 );
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
@@ -109,7 +78,7 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.0f, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		ourShader.Use();
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
